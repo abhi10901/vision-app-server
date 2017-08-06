9b371d4ed9d12038e53c2cd37fdcefc6a448b66c
abhishek bhandari <abhishek bhandari>
2017-08-06 12:36:25 -0700

    Removed console logs

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 require("dotenv").config();
 const Server = require("./server");
 
-console.log(process.env.PORT);
-
 Server.start(process.env.PORT || 4000, function(err) {
-    
+
     if(err) console.log("Server failed to start.");
 })
